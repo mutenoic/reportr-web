bb05d644a29f2e34d88a59be4d8421ce1494aa69
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-04-08 22:02:50 +0300

    Add pages

---
+++ pages/dashboard.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Dashboard layout</h1>
+
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Index Page/ Welcome/ Product ad</h1>
+  </div>
+</template>
+
+<style lang="scss"></style>
