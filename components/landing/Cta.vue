ffa7951cb37c07da6b8bf84d5ee0c79e649bee82
Packata681056 <ribovr@gmail.com>
2024-04-26 12:15:51 +0300

    Added reportr image and added download link

--- components/landing/Cta.vue
+++ components/landing/Cta.vue
@@ -1,26 +1,31 @@
 <template>
   <div
-    class="bg-gradient-to-r from-black to-gray-800 px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center shadow-lg transform transition duration-500 hover:scale-105"
-  >
+    class="bg-gradient-to-r from-black to-gray-800 px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center shadow-lg transform transition duration-500 hover:scale-105">
     <h2 class="text-white text-3xl md:text-6xl font-bold tracking-wider animate-pulse">
       SIGN UP YOUR COMPANY TODAY!
     </h2>
     <p class="text-gray-300 mt-4 text-lg md:text-xl leading-relaxed">
-      Fast and easy way to create and share reports with your team and clients in minutes. 
+      Fast and easy way to create and share reports with your team and clients in minutes.
     </p>
     <div class="flex mt-5 space-x-3">
-      <LandingLink href="#" styleName="inverted" class="bg-white text-black py-2 px-4 rounded-md shadow hover:bg-gray-50 transition duration-300">
-        Download the app now!
-      </LandingLink>
+      <a href="/reportr.apk">
+        <Button styleName="inverted"
+          class="bg-white text-black py-2 px-4 rounded-md shadow hover:bg-gray-50 transition duration-300">
+          Download the app now!
+        </Button>
+      </a>
     </div>
   </div>
 </template>
 
 <style>
 @keyframes pulse {
-  0%, 100% {
+
+  0%,
+  100% {
     opacity: 1;
   }
+
   50% {
     opacity: .5;
   }
--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 
-const logoUrl = "https://cdn.discordapp.com/attachments/1045463014876397638/1231993378254885015/mm123.png?ex=6638fa3e&is=6626853e&hm=4a8d0bac4fa67698051f1e01b8c18d9ecd82bbaef965285eabc00998a3b49c94&";
 const menuItems = [
   { title: "Pricing", path: "#pricing" },
   { title: "About", path: "#about" },
@@ -36,8 +35,8 @@ const isShrunk = computed(() => scrollY.value > 50);
 <template>
   <div class="sticky top-3 flex justify-center z-30">
 
-    <img :src="logoUrl" @click="scrollToHero"
-      :class="`${isShrunk ? 'h-12 lg:h-20' : 'h-16 lg:h-24'} transition-all duration-300 ease-in-out rounded-full shadow hover:cursor-pointer bg-white`"
+    <img src="/reportr-logo.png" @click="scrollToHero"
+      :class="`${isShrunk ? 'h-12 lg:h-20' : 'h-16 lg:h-24'} transition-all duration-300 ease-in-out rounded-full shadow hover:cursor-pointer bg-black p-1`"
       alt="Logo">
 
   </div>
